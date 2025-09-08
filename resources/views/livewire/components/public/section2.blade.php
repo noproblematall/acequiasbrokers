d5d6a965a70e482ce53c346e2704c3fb00446ba5
noproblematall <aragon.jefferson89@gmail.com>
2025-09-08 14:43:00 +0000

    update section 2

--- resources/views/livewire/components/public/section2.blade.php
+++ resources/views/livewire/components/public/section2.blade.php
@@ -3,7 +3,7 @@
         <div class="mb-4" style="background-color: {{ $section_2_bg_color }};">
             <div class="flex items-center p-2 gap-2">
                 <!-- Logo Section (1 part width) -->
-                <div class="w-1/3">
+                <div>
                     <div class="flex justify-center items-center h-full">
                         <svg class="w-20 h-20 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
                             <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path>
@@ -12,7 +12,7 @@
                 </div>
                 
                 <!-- Content Section (2 parts width) -->
-                <div class="w-2/3">
+                <div class="flex-1">
                     @if($section_2_line_1)
                         <h2 class="mb-2 {{ $section_2_line_1_font_weight }} {{ $section_2_line_1_font_size }} {{ $section_2_line_1_text_alignment }}" style="font-family: {{ $section_2_line_1_font_name }}; color: {{ $section_2_line_1_font_color }};">
                             {{ $section_2_line_1 }}
